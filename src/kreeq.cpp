e42bf17e1c149b49af3f422f5d29f20cb48fb45a
gf777 <giulio.formenti@gmail.com>
2023-06-20 05:15:33 -0400

    account for loading existing map

--- src/kreeq.cpp
+++ src/kreeq.cpp
@@ -279,7 +279,7 @@ bool DBG::updateMap(std::string prefix, uint16_t m) {
     phmap::BinaryOutputArchive ar_out(prefix.c_str()); // dumps the data
     dumpMap.phmap_dump(ar_out);
     
-    uint64_t map_size = map[m].size() * (sizeof(DBGkmer) + sizeof(uint64_t));
+    uint64_t map_size = map[m].size() * (sizeof(DBGkmer) + sizeof(uint64_t)) - dumpMap.size() * (sizeof(DBGkmer) + sizeof(uint64_t));
     
 //    freeContainer(map[m]);
     

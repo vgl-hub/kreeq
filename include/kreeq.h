b5bbe83710f75e1be5acfaf09a7a47f997d516ac
gf777 <giulio.formenti@gmail.com>
2023-07-22 11:11:32 -0400

    Update kreeq.h

--- include/kreeq.h
+++ include/kreeq.h
@@ -24,7 +24,7 @@ class DBG : public Kmap<UserInputKreeq, DBGkmer, kmer> {
     std::atomic<bool> readingDone{false}, dumpMaps{false};
     std::vector<std::thread> threads;
     std::vector<std::future<bool>> futures;
-    uint8_t hashThreads = 5;
+    uint8_t hashThreads = 6;
     std::mutex hashMtx;
     
     UserInputKreeq& userInput;

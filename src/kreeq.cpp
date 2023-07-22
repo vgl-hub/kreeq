8d95f43743bd04f2c419b0feae3b579ea7754136
gf777 <giulio.formenti@gmail.com>
2023-07-22 01:37:58 -0400

    Update kreeq.cpp

--- src/kreeq.cpp
+++ src/kreeq.cpp
@@ -266,14 +266,12 @@ bool DBG::processBuffers(std::array<uint16_t, 2> mapRange) {
             
             if (i >= mapRange[0] && i < mapRange[1]) {
 
-                phmap::flat_hash_map<uint64_t, DBGkmer>* thisMap = maps[i]; // the map associated to this buffer
+                phmap::flat_hash_map<uint64_t, DBGkmer>& thisMap = *maps[i]; // the map associated to this buffer
 //                std::cout<<i<<std::endl;
                 
                 uint64_t pos = 1;
-                DBGkmer dbgkmer;
-                std::pair<uint64_t,DBGkmer> pair(pos,dbgkmer);
                 
-                thisMap->insert(pair);
+                thisMap[pos];
 //
 //                for (uint64_t w = 0; w<4; ++w) { // update weights
 //

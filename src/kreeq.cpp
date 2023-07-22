c160b7815b0d5be363077309a276e56547c7ca79
gf777 <giulio.formenti@gmail.com>
2023-07-22 02:21:53 -0400

    Update kreeq.cpp

--- src/kreeq.cpp
+++ src/kreeq.cpp
@@ -164,7 +164,7 @@ bool DBG::hashSequences(uint8_t t) {
             kmer &khmer = buf->seq[buf->pos++];
             
             if(buf->pos > 10000000)
-                std::cout<<"ARGH!!"<<std::endl;
+                std::cout<<"ARGH!!: "<<kcount<<std::endl;
             
             khmer.hash = hash(str+p, &isFw);
             

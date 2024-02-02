68d24d4de93ed8af509b47becefe1d2231b90278
gf777 <giulio.formenti@gmail.com>
2024-02-02 01:44:01 +0100

    Update kreeq.cpp

--- src/kreeq.cpp
+++ src/kreeq.cpp
@@ -290,7 +290,7 @@ bool DBG::buffersToMaps() {
     
     for(uint16_t b = 0; b<mapCount; ++b) {
         std::string fl = userInput.prefix + "/.buf." + std::to_string(b) + ".bin";
-        allocMemory(fileSize(fl));
+        allocMemory(fileSize(fl)*2);
         threadPool.queueJob([this, b] { return processBuffers(b); });
     }
     
@@ -349,7 +349,7 @@ bool DBG::processBuffers(uint16_t m) {
         
     }
     
-    alloc += mapSize(*maps[m]) - flSize;
+    alloc += mapSize(*maps[m]) - flSize*2;
     
     dumpMap(userInput.prefix, m); // if it does, dump map
     

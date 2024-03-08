60c0077b9c5f59629fd42796e1da9fc678dcb636
gf777 <giulio.formenti@gmail.com>
2024-03-08 09:36:53 -0500

    increase default mem limit

--- src/main.cpp
+++ src/main.cpp
@@ -360,7 +360,7 @@ int main(int argc, char **argv) {
     Input in;
     
     threadPool.init(maxThreads); // initialize threadpool
-    maxMem = (userInput.maxMem == 0 ? get_mem_total(3) * 0.5 : userInput.maxMem); // set memory limit
+    maxMem = (userInput.maxMem == 0 ? get_mem_total(3) * 0.9 : userInput.maxMem); // set memory limit
     
     in.loadInput(userInput); // load user input
     lg.verbose("User input loaded");

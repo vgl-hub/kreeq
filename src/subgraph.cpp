ca8c0f0d6fb9ccf003af37fb66d2ef3e9e80e32f
gf777 <giulio.formenti@gmail.com>
2024-06-23 11:36:13 -0400

    Update subgraph.cpp

--- src/subgraph.cpp
+++ src/subgraph.cpp
@@ -438,7 +438,7 @@ void DBG::bestFirst() {
                 auto results = dijkstra(pair, mapRange);;
                 explored += results.first;
                 if (results.first) {
-                    candidates->insert(results.second.begin(), results.second.end());
+//                    candidates->insert(results.second.begin(), results.second.end());
                     DBGsubgraphCpy.erase(pair.first);
                 }
             }

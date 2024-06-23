69da7c322ccbde53d23b01f8d60fd881f940f368
gf777 <giulio.formenti@gmail.com>
2024-06-23 16:59:45 -0400

    Update subgraph.cpp

--- src/subgraph.cpp
+++ src/subgraph.cpp
@@ -545,10 +545,10 @@ std::pair<bool,ParallelMap32color> DBG::dijkstra(std::pair<uint64_t,DBGkmer32col
             }
         }
     }
-    if (explored) {
-        for (auto node : dist) // clear the cache for this source
-            graphCache->erase(node.first);
-    }
+//    if (explored) {
+//        for (auto node : dist) // clear the cache for this source
+//            graphCache->erase(node.first);
+//    }
     return std::make_pair(explored,discoveredNodes);
 }
 

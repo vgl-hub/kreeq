afd39dd6f9df0a63b9b095813e616aec130bde12
gf777 <giulio.formenti@gmail.com>
2024-06-23 02:22:48 -0400

    Update subgraph.cpp

--- src/subgraph.cpp
+++ src/subgraph.cpp
@@ -530,7 +530,7 @@ std::pair<bool,ParallelMap32color> DBG::dijkstra(std::pair<uint64_t,DBGkmer32col
             }
         }
         depth += 1;
-        if(edgeCount == exploredCount || depth == userInput.kmerDepth + 1)
+        if(edgeCount == exploredCount || depth == userInput.kmerDepth + 1 || destinations.size() == 10)
             explored = true;
     }
     ParallelMap32color discoveredNodes;
